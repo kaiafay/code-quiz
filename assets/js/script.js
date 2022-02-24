18a5f1224947afda3837874b5c734d9f3404b0d8
Kaia Scheirman <kfscheirman18@gmail.com>
2022-02-23 18:04:27 -0800

    add an array for questions, choices, and answers

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        prompt: "Which is NOT a data type in JavaScript?",
+        choices: ["Boolean", "Numbers", "String", "Alerts"],
+        answer: "Alerts"
+    },
+    {
+        prompt: "An array is enclosed in __________.",
+        choices: ["Curly brackets", "Parantheses", "Square brackets", "Quotations"],
+        answer: "Square brackets"
+    },
+    {
+        prompt: "Which is referred to as the assignment operator?",
+        choices: ["=", "==", "!", "++"],
+        answer: "="
+    },
+    {
+        prompt: "A function can be nested inside another function.",
+        choices: ["True", "False"],
+        answer: "True"
+    },
+    {
+        prompt: "A variable declared using 'const' can later be reassigned.",
+        choices: ["True", "False"],
+        answer: "False"
+    }
+];
